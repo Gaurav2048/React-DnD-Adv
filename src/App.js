f52707e39af368dd81f58035df2063db75b625a2
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-07-24 20:39:05 +0530

    Implement chapter drag and drop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useRef, useState } from 'react';
-import List from './Components/List/List';
 import ListItem from './Components/List/ListItem';
 
 const App = () => {
   const draggingTopic = useRef();
   const dragOverTopic = useRef();
 
-  const [isChapterDragging, setIsChapterDragging] = useState(false);
+  const draggingChapter = useRef();
+  const dragOverChapter = useRef();
+
+  const [isChapterDragging, setIsChapterDragging] = useState(true);
 
   const [data, setData] = useState([
     {
@@ -53,10 +55,12 @@ const App = () => {
 
   const handleTopicDragStart = (e, { chapterIndex, topicIndex }) => {
     console.log('handle started');
+    setIsChapterDragging(false);
     draggingTopic.current = { chapterIndex, topicIndex };
   };
 
   const handleTopicDragEnd = (e) => {
+    setIsChapterDragging(true);
     console.log(draggingTopic.current, dragOverTopic.current);
     let dataCopy = JSON.parse(JSON.stringify(data));
     if (
@@ -108,6 +112,32 @@ const App = () => {
     dragOverTopic.current = { chapterIndex, topicIndex };
   };
 
+  const handleChapterDragStart = (e, chapterIndex) => {
+    draggingChapter.current = chapterIndex;
+  };
+
+  const handleChapterDragEnd = (e) => {
+    if (!isChapterDragging) return;
+    const copyData = JSON.parse(JSON.stringify(data));
+    const draggingIndex = draggingChapter.current;
+    const draggedOverIndex = dragOverChapter.current;
+
+    if (draggingIndex > draggedOverIndex) {
+      copyData.splice(draggedOverIndex, 0, copyData[draggingIndex]);
+      copyData.splice(draggingIndex + 1, 1);
+    } else if (draggingIndex < draggedOverIndex) {
+      copyData.splice(draggedOverIndex + 1, 0, copyData[draggingIndex]);
+      copyData.splice(draggingIndex, 1);
+    }
+    draggingChapter.current = null;
+    dragOverChapter.current = null;
+    setData(copyData);
+  };
+
+  const handleChapterDragEnter = (e, chapterIndex) => {
+    dragOverChapter.current = chapterIndex;
+  };
+
   return (
     <div>
       <ListItem>
@@ -116,6 +146,10 @@ const App = () => {
             <ListItem.Chapter
               chapterTitle={chapter.chapterTitle}
               key={chapterIndex}
+              chapterIndex={chapterIndex}
+              onDragStart={handleChapterDragStart}
+              onDragEnd={handleChapterDragEnd}
+              onDragEnter={handleChapterDragEnter}
             >
               {chapter.topics.map((topic, topicIndex) => (
                 <ListItem.Chapter.Topic
--- src/Components/List/Chapter.js
+++ src/Components/List/Chapter.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Topic from './Topic';
 
-const Chapter = ({ chapterTitle, ...props }) => {
+const Chapter = ({
+  chapterTitle,
+  chapterIndex,
+  onDragStart,
+  onDragEnd,
+  onDragEnter,
+  ...props
+}) => {
   return (
     <div>
       <div
@@ -9,6 +16,11 @@ const Chapter = ({ chapterTitle, ...props }) => {
         draggable="true"
         style={{
           borderBottom: '.5px solid #EEEEEE',
+        }}
+        onDragStart={(e) => onDragStart(e, chapterIndex)}
+        onDragEnd={onDragEnd}
+        onDragEnter={(e) => {
+          onDragEnter(e, chapterIndex);
         }}
       >
         <h2> {chapterTitle} </h2>
