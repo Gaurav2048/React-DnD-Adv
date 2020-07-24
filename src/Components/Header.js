e5d9a94891bc245985b2c2197d6a395481221a57
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-07-25 01:55:23 +0530

    Implement subject name add feature to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Header = () => {
-  return <div className="header">Header</div>;
+const Header = ({ subject, setSubject }) => {
+  return (
+    <div className="header">
+      <input
+        placeholder="SUBJECT NAME"
+        className="subject_name"
+        value={subject}
+        onChange={(e) => setSubject(e.value)}
+      />
+      <div style={{ borderBottom: '.5px solid #EEEEEE', width: '100%' }}></div>
+    </div>
+  );
 };
 
 export default Header;
