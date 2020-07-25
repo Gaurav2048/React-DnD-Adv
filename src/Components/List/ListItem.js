63b6ba3cfa64d3e0292fa365a77124a615bad8f4
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-07-25 22:18:52 +0530

    Implement add new chapter feature

--- src/Components/List/ListItem.js
+++ src/Components/List/ListItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Chapter from './Chapter';
 import SubHeader from '../SubHeader';
+import AddChapter from '../AddChapter';
 
 const ListItem = (props) => {
   return <div>{props.children}</div>;
 };
 
+ListItem.AddChapter = AddChapter;
 ListItem.SubHeader = SubHeader;
 ListItem.Chapter = Chapter;
 
